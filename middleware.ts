0c5fbe8b00d18222df2ff587296deb80495f2f4d
apex-devteam <devteam@apexpostech.com>
2024-03-12 11:01:01 -0700

    Added ENV variables in production

--- middleware.ts
+++ middleware.ts
@@ -5,25 +5,7 @@ import { signIn } from "next-auth/react";
 
 
 export async function middleware (req: NextRequest) {
-    // if (req.nextUrl.searchParams.get("request_token")) {
-    //     // const requestToken = req.nextUrl.searchParams.get("request_token")
-    //     // const session = await postData("/authentication/session/new", {
-    //     //     request_token: requestToken
-    //     // })
 
-    //     // const signinInfo = await handleSignin(session)
-    //     // console.log("signininfo,", signinInfo)
-    //     // // const handleSignin = async () => {
-    //     //     "use client"
-    //     //     const signinResponse = await signIn("credentials", {
-    //     //         username: session.session_id,
-    //     //         redirect: false
-    //     //     })
-    //     //     return signinResponse
-    //     // }
-    //     // const signinResponse = await handleSignin()
-    //     // console.log("SIGNINRESPONSE", signinInfo)
-    // }
     
 };
 
